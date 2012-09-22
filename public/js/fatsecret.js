a9a9771b29d46f72c7a3c8b966e6a071206d07b5
Brian Yang <brianyang1@gmail.com>
2012-09-22 14:54:02 -0400

    local js

---
+++ public/js/fatsecret.js
@@ -0,0 +1,47 @@
+
+var fatsecret = {
+	variables: {
+		
+		rootUrl: 'http://platform.fatsecret.com/js/Default.aspx?pg=',
+		
+		splitter: 'fs_a3315bb6fc9b4771b89f77e6059d0cf1',
+		key: '05db11a0bba740389461cfbd45571f97',
+		appTitle: 'My Diet',
+		navOptions: 31,
+		
+		autoLoad: true,
+		autoTemplate: true
+	},
+	
+	setCookie: function(name, value){
+		var date = new Date();
+		date.setTime(date.getTime()+(10*365*24*60*60*1000));		
+		document.cookie = name + '=' + encodeURIComponent(value) + ';expires=' + date.toUTCString() + ';path=/';
+	},
+	
+	doWrite: function(url, isCss){
+		var h = isCss ? '<link type="text/css" rel="stylesheet"' : '<script type="text/javascript"'; 
+		h += (isCss ? ' href' : ' src') + '="' + url; 
+		h += isCss ? '"/>' : '"></script>'; 
+		document.write(h);
+	},
+	
+	start: function(){
+		this.doWrite('http://platform.fatsecret.com/js/static/css/all.css', true);
+		
+		
+			this.doWrite('http://platform.fatsecret.com/js/static/css/themes/blue.css', true);
+		
+		
+		this.doWrite('http://platform.fatsecret.com/js/static/script/final.js');
+		
+		
+		
+		
+		
+	}
+};
+fatsecret.start();
+
+
+	document.write('<div class="fatsecret_container" id="fatsecret_container"></div>');
