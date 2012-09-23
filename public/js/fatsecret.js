741fe92742397383ed073d4457c9168b7ba1d60e
Brian Yang <brianyang1@gmail.com>
2012-09-22 20:53:15 -0400

    local styles and js api

--- public/js/fatsecret.js
+++ public/js/fatsecret.js
@@ -1,44 +1,40 @@
 
 var fatsecret = {
 	variables: {
-		
+
 		rootUrl: 'http://platform.fatsecret.com/js/Default.aspx?pg=',
-		
+
 		splitter: 'fs_a3315bb6fc9b4771b89f77e6059d0cf1',
 		key: '05db11a0bba740389461cfbd45571f97',
 		appTitle: 'My Diet',
 		navOptions: 31,
-		
+
 		autoLoad: true,
 		autoTemplate: true
 	},
-	
+
 	setCookie: function(name, value){
 		var date = new Date();
-		date.setTime(date.getTime()+(10*365*24*60*60*1000));		
+		date.setTime(date.getTime()+(10*365*24*60*60*1000));
 		document.cookie = name + '=' + encodeURIComponent(value) + ';expires=' + date.toUTCString() + ';path=/';
 	},
-	
+
 	doWrite: function(url, isCss){
-		var h = isCss ? '<link type="text/css" rel="stylesheet"' : '<script type="text/javascript"'; 
-		h += (isCss ? ' href' : ' src') + '="' + url; 
-		h += isCss ? '"/>' : '"></script>'; 
+		var h = isCss ? '<link type="text/css" rel="stylesheet"' : '<script type="text/javascript"';
+		h += (isCss ? ' href' : ' src') + '="' + url;
+		h += isCss ? '"/>' : '"></script>';
 		document.write(h);
 	},
-	
+
 	start: function(){
-		this.doWrite('http://platform.fatsecret.com/js/static/css/all.css', true);
-		
-		
-			this.doWrite('http://platform.fatsecret.com/js/static/css/themes/blue.css', true);
-		
-		
-		this.doWrite('http://platform.fatsecret.com/js/static/script/final.js');
-		
-		
-		
-		
-		
+		this.doWrite('/css/all.css', true);
+			this.doWrite('/css/blue.css', true);
+		this.doWrite('/js/final.js');
+
+
+
+
+
 	}
 };
 fatsecret.start();
